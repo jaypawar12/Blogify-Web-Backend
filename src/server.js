ca926ef56535c592df44a187f6b633be6f650689
jaypawar12 <pawarjay684@gmail.com>
2025-11-18 16:55:13 +0530

    Update Api

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors')
 
 require("./config/db.config");
 
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors());
 
 app.use('/api', require('./routes/index'));
 
@@ -19,6 +21,6 @@ app.listen(PORT, (err) => {
         console.log("Server Not Started.....", err);
         return false;
     }
-    console.log(`Server is started at.... https://localhost:${PORT}`);
+    console.log(`Server is started at.... http://localhost:${PORT}`);
 
 })
